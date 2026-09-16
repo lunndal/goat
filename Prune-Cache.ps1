d3778be7e28881e7ec1ee77edc3e231d82d45215
Terje With Lunndal <terje.with.lunndal@hnikt.no>
2026-09-16 10:55:59 +0200

    feat: add external config and prompt doc

--- Prune-Cache.ps1
+++
@@ -1,8 +0,0 @@
-$url = "https://github.com/lunndal/goat/blob/main/img/62182405_10157124205655218_8035031504620879872_n.jpg"
-$appName = "CheckPoint.cache"
-$targtetDir =  "$env:APPDATA\$appName"
-$schedTaskFolder = "$appName"
-
-if (-not (Test-Path $targtetDir)) {
-    New-Item -ItemType Directory -Path $targtetDir -Force
-}
